1554b636e27c61e6fe649427c423e77b0b65663b
m414rkie <joncmparsons@gmail.com>
2019-08-08 20:37:53 -0700

    Adjusted spellings. Removed deprecated routines.

--- nonlifesubs.f90
+++ nonlifesubs.f90
@@ -1,23 +1,3 @@
-subroutine printer(arr,big)
-! Printing subroutine for arrays of two dimensions
-
-implicit none
-	real,dimension(big,big),intent(in)			:: arr	! Input matix
-	integer,intent(in)							:: big	! Size of matrix
-	integer										:: i, j	! Looping integers
-
-! Format statement
-50 format(5g11.5)
-
-! Writing loop
-do i =1,big
-	write(*,50)(arr(i,j),j=1,big)
-end do
-
-end subroutine
-
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
 subroutine printtofile(arrin,size,file)
 
 ! Prints to a file in x-y-z format
@@ -40,37 +20,6 @@ do i = 1, size, 1
 end do
 
 close(19)
-
-end subroutine
-
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-subroutine printbact(filea,fileb,filec)
-
-! Prints to a file in x-y-int-int format. Column 3 totalpop,
-! Column 4 number of species
-
-use globalvars
-
-implicit none
-	character*50, intent(in)	:: filea, fileb, filec
-	integer 									:: i, j
-
-open(unit=16,file=trim(filea),status="replace",position="append")
-open(unit=17,file=trim(fileb),status="replace",position="append")
-open(unit=18,file=trim(filec),status="replace",position="append")
-
-do i = 1, 2*grid, 1
-	do j = 1, 2*grid, 1
-		write(16,*) i, j, bacteria(i,j)%totalpop, bacteria(i,j)%numspecies
-		write(17,*) i, j, phage(i,j)%totalpop, phage(i,j)%numspecies
-		write(18,*) i, j, lys(i,j)%totalpop, lys(i,j)%numspecies
-	end do
-end do
-
-close(16)
-close(17)
-close(18)
 
 end subroutine
 
